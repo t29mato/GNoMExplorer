38f49d0c3804a7bf2eff2558428f2d94874ff581
t29mato <tomoya.matou@gmail.com>
2023-12-08 16:58:42 +0900

    feat: filter elements

--- src/app/ag-grid-table.js
+++ src/app/ag-grid-table.js
@@ -13,6 +13,8 @@ import Papa from 'papaparse'; // papaparse for parsing CSV data
 export const GridExample = () => {
     // Row Data: The data to be displayed.
     const [rowData, setRowData] = useState([]);
+    const [filterElements, setFilterElements] = useState(''); // User input for elements to filter
+
 
     // Column Definitions
     const colDefs = [
@@ -30,10 +32,12 @@ export const GridExample = () => {
             .then(response => {
                 Papa.parse(response.data, {
                     header: true,
+                    preview: 100,
                     complete: (results) => {
                         setRowData(results.data);
                     }
                 });
+                console.info('The load has finished.')
             })
             .catch(error => console.error('Error fetching data:', error));
     }, []);
@@ -41,13 +45,41 @@ export const GridExample = () => {
 
     const gridStyle = {
         height: '100vh', // 例えば500pxの高さ
-        width: '100%' // 幅はコンテナに合わせる
+        width: '100vw' // 幅はコンテナに合わせる
     };
+
+    // Filter data based on user input
+    const filterData = () => {
+        const elements = filterElements.split(',').map(el => el.trim());
+        console.log({elements})
+        console.log(rowData[0])
+        const filteredData = rowData.filter(row => {
+                let jsonStr = row.Elements.replace(/'/g, '"');
+                return elements.every(el => {
+                    console.log({el})
+                    return JSON.parse(jsonStr).includes(el)
+                })
+            }
+        );
+        setRowData(filteredData);
+    };
+
 
     // Container: Defines the grid's theme & dimensions.
     return (
-        <div style={gridStyle}>
-            <AgGridReact  rowData={rowData} columnDefs={colDefs} />
+        <div>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Enter elements to filter, e.g., Na,Cl"
+                    value={filterElements}
+                    onChange={(e) => setFilterElements(e.target.value)}
+                />
+                <button onClick={filterData}>Filter Data</button>
+            </div>
+            <div style={gridStyle}>
+                <AgGridReact rowData={rowData} columnDefs={colDefs} />
+            </div>
         </div>
     );
 };
