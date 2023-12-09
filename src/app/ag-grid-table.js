795673aba2757e5b9d1ae6e912eef09c5dfe5e4d
t29mato <tomoya.matou@gmail.com>
2023-12-09 09:23:01 +0900

    adjust styles

--- src/app/ag-grid-table.js
+++ src/app/ag-grid-table.js
@@ -14,7 +14,7 @@ export const GridExample = () => {
     // Row Data: The data to be displayed.
     const [rowData, setRowData] = useState([]);
     const [filterElements, setFilterElements] = useState(''); // User input for elements to filter
-    const [isLoading, setIsLoading] = useState(false); // 追加: データ読み込み中の状態
+    const [isLoading, setIsLoading] = useState(true); // 追加: データ読み込み中の状態
     const [isFiltering, setIsFiltering] = useState(false); // 追加: フィルター実行中の状態
 
 
@@ -130,21 +130,26 @@ export const GridExample = () => {
 
     return (
         <div style={{width: '100%'}}>
-            <div>
-                <input
-                    style={inputStyle}
-                    type="text"
-                    placeholder="Enter elements to filter, e.g., Pb,Te"
-                    value={filterElements}
-                    onChange={(e) => setFilterElements(e.target.value)}
-                />
-                <button style={buttonStyle} onClick={filterData}>Filter Data</button>
-            </div>
             {isLoading && <p>Loading data...</p>} {/* データ読み込み中の表示 */}
-            {isFiltering && <p>Filtering data...</p>} {/* フィルター実行中の表示 */}
-            <div className="ag-theme-quartz" style={gridStyle}>
-                <AgGridReact rowData={rowData} columnDefs={colDefs} pagination={true}/>
-            </div>
+            {!isLoading && (
+                <div>
+                    <div>
+                        <input
+                            style={inputStyle}
+                            type="text"
+                            placeholder="Enter elements to filter, e.g., Pb,Te"
+                            value={filterElements}
+                            onChange={(e) => setFilterElements(e.target.value)}
+                        />
+                        <button style={buttonStyle} onClick={filterData}>Filter Data</button>
+                    </div>
+                    <div className="ag-theme-quartz" style={gridStyle}>
+                        <AgGridReact rowData={rowData} columnDefs={colDefs} pagination={true}/>
+                    </div>
+                    {isFiltering && <p>Filtering data...</p>} {/* フィルター実行中の表示 */}
+                </div>
+            )}
+
         </div>
     );
 };
